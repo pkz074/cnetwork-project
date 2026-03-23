073ca6932db677029f640d1cc988d5a3d7c5d552
koairs <kkaahh452@gmail.com>
2026-03-23 18:19:55 -0400

    Add TCP client implementation in hello_client.c

--- hello_client.c
+++ hello_client.c
@@ -1,1 +1,66 @@
+/* TCP hello_client.c*/
+/*run on VM2 with the ip and SERVER_TCP_PORT*/
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <strings.h>
 
+#define SERVER_TCP_PORT 3000
+#define BUFLEN		256
+
+int main(int argc, char **argv)
+{
+	int 	n, sd, port;
+	struct	hostent		*hp;
+	struct	sockaddr_in server;
+	char	*host, rbuf[BUFLEN];
+
+	switch(argc){
+	case 2:
+		host = argv[1];
+		port = SERVER_TCP_PORT;
+		break;
+	case 3:
+		host = argv[1];
+		port = atoi(argv[2]);
+		break;
+	default:
+		fprintf(stderr, "Usage: %s host [port]\n", argv[0]);
+		exit(1);
+	}
+
+	/* Create a stream socket	*/	
+	if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		fprintf(stderr, "Can't creat a socket\n");
+		exit(1);
+	}
+
+	bzero((char *)&server, sizeof(struct sockaddr_in));
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+	if (hp = gethostbyname(host)) 
+	  bcopy(hp->h_addr, (char *)&server.sin_addr, hp->h_length);
+	else if (!inet_aton(host, (struct in_addr *) &server.sin_addr) ){
+	  fprintf(stderr, "Can't get server's address\n");
+	  exit(1);
+	}
+
+	/* Connecting to the server */
+	if (connect(sd, (struct sockaddr *)&server, sizeof(server)) == -1){
+	  fprintf(stderr, "Can't connect \n");
+	  exit(1);
+	}
+
+printf("Waiting for message from server...\n");
+while ((n=read(sd, rbuf, BUFLEN))>0){
+  write(1, rbuf, n);
+}
+
+	close(sd);
+	return(0);
+}
